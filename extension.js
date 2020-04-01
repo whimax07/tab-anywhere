0c2febf41cb3da6652db82d55f0a2a86c134a4e3
Alexander Vassilev <av@mega.co.nz>
2020-04-01 19:44:36 +0300

    Fix indentation bugs when leading whitespace contains both tabs and spaces. Some cleanup. Unify leading whitespace when tab pressed even if no need to fix indent.

--- extension.js
+++ extension.js
@@ -3,29 +3,14 @@ const nextIndentPattern = /.*[\{\[\(\:]\s*(\/\/.*|\/\*.*\*\/\s*|#.*|)$/gm;
 const nextIndentPattern_makefile = /.*[\{\[\(\:]/gm;
 
 function lineRequiresNextIndent(line, doc) {
-    var lineStr = line.text;
-    var matches = lineStr.match((doc.languageId === "makefile")
+    let lineStr = line.text;
+    let matches = lineStr.match((doc.languageId === "makefile")
         ? nextIndentPattern_makefile
         : nextIndentPattern
     );
     return matches;
-
-    let code;
-    let i = lineStr.length-1;
-    for (; i >= 0; i--) {
-        code = lineStr.charCodeAt(i);
-        if (code !== 32 && code !== 9 && code !== 13 || code !== 10) {
-            break;
-        }
-    }
-    if (i < 0) {
-        return false;
-    }
-    return (code === 40)  || // (
-           (code === 91)  || // [
-           (code === 123) || // {
-           (code === 58);    // :
 }
+
 function lineRequiresUnindent(line) {
     if (line.isEmptyOrWhitespace) {
         return false;
@@ -67,7 +52,7 @@ exports.activate = function() {
             if (l.isEmptyOrWhitespace) {
                 continue;
             }
-            var txt = l.text;
+            let txt = l.text;
             let char1 = txt.charCodeAt(l.firstNonWhitespaceCharacterIndex);
             let char2 = txt.charCodeAt(l.firstNonWhitespaceCharacterIndex + 1);
             if ((char1 === 47) && (char2 === 47 || char2 === 42)) { // '//' and '/*'
@@ -92,22 +77,19 @@ exports.activate = function() {
         let prevIndent;
         let indentStep;
 
-        if (insertSpaces) {
-            currIndent = currLine.firstNonWhitespaceCharacterIndex;
-            prevIndent = prevLine.firstNonWhitespaceCharacterIndex;
-            indentStep = indentSize;
-        }
-        else {
-            currIndent = leadWsCount(currLine, tabSize);
-            prevIndent = leadWsCount(prevLine, tabSize);
-            indentStep = tabSize;
-        }
+        currIndent = leadWsCount(currLine, tabSize);
+        prevIndent = leadWsCount(prevLine, tabSize);
+        indentStep = insertSpaces ? indentSize : tabSize;
 
         let target;
         if (lineRequiresUnindent(currLine, doc)) {
             if (lineRequiresNextIndent(prevLine, doc)) {
+                // example {
+                // }
                 target = prevIndent;
             } else {
+                //     example
+                // }
                 target = prevIndent - indentStep;
                 if (target < 0) {
                     target = 0;
@@ -121,47 +103,50 @@ exports.activate = function() {
             target = prevIndent;
         }
 
-        let diff = (currIndent - target); // diff is always in single spaces
-        if (diff === 0 && insertSpaces) {
-            return; // no change
-        }
-        if (!insertSpaces) { // we operate with tabs
-            // diff can be zero here, but we may need to replace spaces with tabs and still have a diff
-            let txt = currLine.text;
-            let end = currLine.firstNonWhitespaceCharacterIndex;
-            for (let i = 0; i < end; i++) {
-                if (txt.charCodeAt(i) !== 9) {
-                    // we work with tabs, but have spaces as well, so delete the whole whitespace from
-                    // start of line and replace it with the calculated number of tabs
-                    editor.edit(function(builder) {
-                        builder.replace(new vscode.Range(cursorLine, 0, cursorLine, end), '\t'.repeat(Math.floor(target/tabSize)));
-                    });
-                    return;
-                }
-            }
-            if (diff === 0) {
+        // unify leading whitespace if it contains indents not of our tab/space mode
+        let txt = currLine.text;
+        let end = currLine.firstNonWhitespaceCharacterIndex;
+        let validWs = insertSpaces ? 32 : 9;
+        // diff can be zero here, but we may need to replace spaces with tabs and still have a diff
+        for (let i = 0; i < end; i++) {
+            if (txt.charCodeAt(i) !== validWs) {
+                // Leading whitespace contains a whitespace char of the opposite type
+                // Replace the whole whitespace from start of line with the proper number of our
+                // ws char
+                editor.edit(function(builder) {
+                    builder.replace(new vscode.Range(cursorLine, 0, cursorLine, end), insertSpaces
+                    ? ' '.repeat(target)
+                    : '\t'.repeat(Math.floor(target/tabSize)));
+                })
+                .then(function() {
+                    let sel = editor.selection;
+                    if (!sel.isEmpty) {
+                        editor.selection = new vscode.Selection(sel.end, sel.end);
+                    }
+                });
                 return;
             }
-            else if (diff < 0) {
-                diff = Math.ceil(diff / tabSize);
-            }
-            else {
-                diff = Math.floor(diff / tabSize);
-            }
+        }
+
+        let diff = (currIndent - target); // diff is always in spaces
+        if (diff === 0) {
+            return; // no change
         }
 
         // business as usual
         if (diff > 0) {
             // overindented - delete some spaces
             editor.edit(function(builder) {
-                builder.delete(new vscode.Range(cursorLine, 0, cursorLine, diff));
+                builder.delete(new vscode.Range(cursorLine, 0, cursorLine,
+                    insertSpaces ? diff : Math.floor(diff / tabSize)));
             });
         }
         else {
-            // under-indented - add some spaces
-            diff = -diff;
+            // diff < 0 : under-indented, add some spaces
             editor.edit(function(builder) {
-                builder.insert(new vscode.Position(cursorLine, 0), (insertSpaces ? ' ': '\t').repeat(diff));
+                builder.insert(new vscode.Position(cursorLine, 0), insertSpaces
+                ? ' '.repeat(-diff)
+                : '\t'.repeat(Math.floor((-diff) / tabSize)));
             });
         }
     } catch(e) {
